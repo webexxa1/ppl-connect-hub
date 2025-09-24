839c1c25470958988022005359909bad7e5efab9
cc <chalama@webexxa.com>
2025-09-23 22:20:40 -0400

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/ppl-connect-hub/", // <-- important for GitHub Pages project sites
+  base: "/", // <-- important for GitHub Pages project sites
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
